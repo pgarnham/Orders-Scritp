a6b4c72f79069e527f3a2093ed35fa6b85002d73
pgarnham <pgarnham@uc.cl>
2020-04-25 04:05:24 -0400

    agregando archivo python correcto, icono y ejecutable exe 32 bits

--- generar_pedidos_e_informe.py
+++ generar_pedidos_e_informe.py
@@ -3,13 +3,22 @@
 import os
 import time
 
+import tkinter.filedialog
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+import pandas as pd
+
+excel_path = tkinter.filedialog.askopenfilename()
+read_file = pd.read_excel(rf"{excel_path}", sheet_name=0)
+read_file.to_csv(r't.csv', index=None, header=True, encoding='utf8', sep=";")
+
+
+# archivo_elegido = filedialog.askopenfilename()
 
 
 pedidos = []
 contador = 0
-with open("plantilla.csv", encoding='utf-8') as file:
+with open("t.csv", "r", encoding="utf-8") as file:
     for linea in file:
         if contador == 0:
             header = linea.split(";")
@@ -24,7 +33,7 @@ with open("plantilla.csv", encoding='utf-8') as file:
             for dato in pedido:
                 if (dato != "\n"):
                     lista_pedido.append(dato)
-                    pedidos.append(lista_pedido)
+            pedidos.append(lista_pedido)
 
 pedidos_en_tuplas = []
 for lista in pedidos:
@@ -34,10 +43,12 @@ for lista in pedidos:
 
 lista_definitiva = []
 
+print(len(pedidos_en_tuplas))
+
 for pedido in pedidos_en_tuplas:
     pedido_final = []
     for tupla in pedido:
-        if tupla[1] != "":
+        if (tupla[1] != "") and (tupla[0] != "nro_pedido"):
             pedido_final.append(tupla)
     lista_definitiva.append(pedido_final)
 
@@ -45,13 +56,16 @@ for pedido in pedidos_en_tuplas:
 horario_ = time.strftime("%H %M %S")  # Formato de 24 horas
 fecha = time.strftime("%d-%m-%y")
 nombre_carpeta = f"Pedidos {fecha} {horario_}"
-os.mkdir("pedidos_pdf/" + nombre_carpeta)
+if not os.path.exists("pedidos_pdf"):
+    os.mkdir("pedidos_pdf")
+os.mkdir("pedidos_pdf/" + nombre_carpeta + "/")
 
 
+nro_pedido = 1
 for pedido_ in lista_definitiva:
     carp_1 = "pedidos_pdf/"
     ruta = carp_1 + nombre_carpeta
-    direccion = ruta + "/pedido_" + pedido_[1][1] + ".pdf"
+    direccion = ruta + "/pedido_" + str(nro_pedido) + "_" + pedido_[2][1] + ".pdf"
     c = canvas.Canvas(direccion, pagesize=letter)
     c.setLineWidth(.3)
     c.setFont('Helvetica-Bold', 12)
@@ -62,13 +76,13 @@ for pedido_ in lista_definitiva:
     aux = 1
     aux_2 = 0
     linea_sig = 15  # Decremento normal para siguiente linea.
-
+    contador = 0
     for tupla in pedido_:
-        if aux <= 5:
+        if aux <= 6:
             c.drawString(horiz_izq, linea, f"{tupla[0]}:  {tupla[1]}")
 
             aux += 1
-            if aux == 6:
+            if aux == 7:
                 c.line(horiz_izq, linea - 2, 500, linea - 2)
                 linea -= (linea_sig + 5)
             else:
@@ -80,7 +94,16 @@ for pedido_ in lista_definitiva:
             c.drawString(horiz_izq, linea, f"{tupla[0]}")
             c.drawString(horiz_der, linea, f"{tupla[1]}")
             c.rect(520, linea - 2, 70, 15)
+            contador = contador + 1
             linea -= 20
+
+        if contador > 28:
+            contador = 0
+            c.showPage()
+            linea = 750  # Donde comienza la primera linea
+            horiz_izq = 40  # Donde se ubica la lista de productos a la izq.
+            horiz_der = 450  # Donde se ubica la lista de cantidades a la der.
+            linea_sig = 15  # Decremento normal para siguiente linea.
     c.drawString(horiz_izq, linea, "Envío a Domicilio")
     c.drawString(horiz_der, linea, "1 envío")
     c.drawString(530, linea, "$ 1.500")
@@ -91,3 +114,6 @@ for pedido_ in lista_definitiva:
     c.drawString(70, linea, "TOTAL")
     c.rect(horiz_der, linea - 10, 150, 25)
     c.save()
+    nro_pedido += 1
+
+os.remove("t.csv")
